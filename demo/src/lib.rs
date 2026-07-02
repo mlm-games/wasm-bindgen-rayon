40e14b91451de498b19d5fa367e78294a58e09b9
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-07-02 18:46:16 +0530

    chore: start example migration

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,23 +1,10 @@
-/*
- * Copyright 2022 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 use hsl::HSL;
+use js_sys::Date;
 use num_complex::Complex64;
 use rand::Rng;
 use rayon::prelude::*;
 use wasm_bindgen::{prelude::*, Clamped};
-
-#[cfg(feature = "parallel")]
-pub use wasm_bindgen_rayon::init_thread_pool;
+use web_sys::{CanvasRenderingContext2d, ImageData};
 
 type RGBA = [u8; 4];
 
@@ -36,7 +23,7 @@ impl Generator {
             width,
             height,
             palette: (0..=max_iterations)
-                .map(move |_| {
+                .map(|_| {
                     let (r, g, b) = HSL {
                         h: rng.gen_range(0.0..360.0),
                         s: 0.5,
@@ -49,11 +36,10 @@ impl Generator {
         }
     }
 
-    #[allow(clippy::many_single_char_names)]
     fn get_color(&self, x: u32, y: u32) -> &RGBA {
         let c = Complex64::new(
-            (f64::from(x) - f64::from(self.width) / 2.0) * 4.0 / f64::from(self.width),
-            (f64::from(y) - f64::from(self.height) / 2.0) * 4.0 / f64::from(self.height),
+            (x as f64 - self.width as f64 / 2.0) * 4.0 / self.width as f64,
+            (y as f64 - self.height as f64 / 2.0) * 4.0 / self.height as f64,
         );
         let mut z = Complex64::new(0.0, 0.0);
         let max = self.palette.len() - 1;
@@ -65,35 +51,114 @@ impl Generator {
         &self.palette[i]
     }
 
-    fn iter_row_bytes(&self, y: u32) -> impl '_ + Iterator<Item = u8> {
-        (0..self.width)
-            .flat_map(move |x| self.get_color(x, y))
-            .copied()
+    fn render_par(&self) -> Vec<u8> {
+        let row_len = self.width as usize * 4;
+        let mut result = vec![0u8; row_len * self.height as usize];
+        result
+            .par_chunks_mut(row_len)
+            .enumerate()
+            .for_each(|(y, row)| {
+                for x in 0..self.width {
+                    let color = self.get_color(x, y as u32);
+                    let offset = x as usize * 4;
+                    row[offset..offset + 4].copy_from_slice(color);
+                }
+            });
+        result
     }
 
-    fn iter_bytes(&self) -> impl '_ + ParallelIterator<Item = u8> {
-        (0..self.height)
-            // Note: when built without atomics, into_par_iter() will
-            // automatically fall back to single-threaded mode.
-            .into_par_iter()
-            .flat_map_iter(move |y| self.iter_row_bytes(y))
+    fn render_seq(&self) -> Vec<u8> {
+        let mut result = vec![0u8; self.width as usize * self.height as usize * 4];
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let color = self.get_color(x, y);
+                let offset = (y * self.width + x) as usize * 4;
+                result[offset..offset + 4].copy_from_slice(color);
+            }
+        }
+        result
     }
 }
 
-#[wasm_bindgen]
-pub fn generate(width: u32, height: u32, max_iterations: u32) -> Result<Clamped<Vec<u8>>, JsValue> {
-    let pixels = u64::from(width)
-        .checked_mul(u64::from(height))
-        .and_then(|n| n.checked_mul(4))
-        .ok_or_else(|| JsValue::from_str("Image dimensions overflow"))?;
+fn draw(canvas_id: &str, time_id: &str, parallel: bool) -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .get_element_by_id(canvas_id)
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let width = canvas.width();
+    let height = canvas.height();
+    let ctx = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()?;
 
-    if pixels > 256 * 1024 * 1024 {
-        return Err(JsValue::from_str("Image too large"));
+    if parallel {
+        let n = web_sys::window().unwrap().navigator().hardware_concurrency() as usize;
+        let threads = n.max(1).min(32);
+        let _ = wasm_bindgen_rayon::init_thread_pool(threads);
     }
 
-    Ok(Clamped(
-        Generator::new(width, height, max_iterations)
-            .iter_bytes()
-            .collect(),
-    ))
+    let gen = Generator::new(width, height, 1000);
+    let start = Date::now();
+
+    let pixels = if parallel {
+        gen.render_par()
+    } else {
+        gen.render_seq()
+    };
+
+    let elapsed = Date::now() - start;
+
+    let img_data = ImageData::new_with_u8_clamped_array(Clamped(&pixels), width)?;
+    ctx.put_image_data(&img_data, 0.0, 0.0)?;
+
+    let output = document.get_element_by_id(time_id).unwrap();
+    output.set_text_content(Some(&format!("{:.2} ms", elapsed)));
+
+    Ok(())
+}
+
+#[wasm_bindgen(start)]
+fn start() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+
+    let single_btn = document
+        .get_element_by_id("singleThread")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlButtonElement>()?;
+    single_btn.remove_attribute("disabled")?;
+
+    let multi_btn = document
+        .get_element_by_id("multiThread")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlButtonElement>()?;
+
+    let is_isolated = js_sys::Reflect::get(
+        &web_sys::window().unwrap(),
+        &"crossOriginIsolated".into(),
+    )
+    .ok()
+    .and_then(|v| v.as_bool())
+    .unwrap_or(false);
+
+    if !is_isolated {
+        multi_btn.set_value("Multi-threaded (requires COOP/COEP headers)");
+    } else {
+        multi_btn.remove_attribute("disabled")?;
+    }
+
+    let cb = Closure::wrap(Box::new(move || {
+        let _ = draw("canvas", "time", false);
+    }) as Box<dyn FnMut()>);
+    single_btn.add_event_listener_with_callback("click", cb.as_ref().unchecked_ref())?;
+    cb.forget();
+
+    let cb2 = Closure::wrap(Box::new(move || {
+        let _ = draw("canvas", "time", true);
+    }) as Box<dyn FnMut()>);
+    multi_btn.add_event_listener_with_callback("click", cb2.as_ref().unchecked_ref())?;
+    cb2.forget();
+
+    Ok(())
 }
